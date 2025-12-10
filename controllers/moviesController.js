dbf224b99cd0788c582397667b3f828340fbe71e
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-12-10 19:42:36 +0100

    fix SHOW query to avoid SQL injections

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -22,10 +22,11 @@ const show = (req, res) => {
     // recupero l'id dall'URL
     const id = req.params.id
 
-    const sql = `SELECT * FROM movies WHERE id = ${id}`
+    const sql = 'SELECT * FROM movies WHERE id = ?'
 
-    connection.query(sql, (err, results) => {
+    connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: 'Database query failed' })
+        if (results.length === 0) return res.status(404).json({ error: 'Movie not found' })
         res.json(results)
     })
 }
