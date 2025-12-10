fc987ac58ca8f8ce4bc648cd0b6fe496e70cb81e
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-12-10 19:25:07 +0100

    add first query for index (movie list)

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,9 +1,18 @@
+const connection = require('../data/db');
 const db = require('../data/db')
 
 // INDEX
 const index = (req, res) => {
     console.log('Richiesta ricevuta: INDEX');
-    res.send('Movie list')
+
+    // prapato la query
+    const sql = 'SELECT * FROM movies'
+
+    // eseguo la query
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        res.json(results)
+    })
 }
 
 // SHOW
